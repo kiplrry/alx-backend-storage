81cedb11200cc15dceab492e8fc611efba7e23f8
kiplrry <kiplarry19@gmail.com>
2024-05-14 15:30:25 +0300

    8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,11 +2,8 @@
 """
 script to gets all docs in a collection
 """
-from typing import List, Any
-from pymongo.cursor import Cursor
-from pymongo.collection import Collection
 
 
-def list_all(mongo_collection: Collection[Any]) -> Cursor[Any]:
+def list_all(mongo_collection):
     """lists all documents in a collection"""
     return mongo_collection.find()
