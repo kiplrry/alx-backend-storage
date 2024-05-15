ec2a21425dfc5e188875b51d8ab391112027e060
kiplrry <kiplarry19@gmail.com>
2024-05-15 15:29:44 +0300

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,10 +9,12 @@ from uuid import uuid4
 class Cache():
     """Cache class"""
     def __init__(self) -> None:
+        """initiates the class with a redis instance"""
         self._redis = Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, float, int, bytes]) -> str:
+        """Stores a data with a random key"""
         key = str(uuid4())
         self._redis.set(key, data)
         return key
