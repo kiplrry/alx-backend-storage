75d8bf2c25855b96281c05f5f5c2cf6bdb6cd7cd
kiplrry <kiplarry19@gmail.com>
2024-05-15 15:35:07 +0300

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 """module implementing the cache class"""
 
-from redis import Redis
+import redis
 from typing import Union
 from uuid import uuid4
 
 
 class Cache():
     """Cache class"""
-    def __init__(self) -> None:
+    def __init__(self):
         """initiates the class with a redis instance"""
-        self._redis = Redis()
+        self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, float, int, bytes]) -> str:
