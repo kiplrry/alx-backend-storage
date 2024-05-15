ff323556aa5555eedfb18f3b397110d4be471e40
kiplrry <kiplarry19@gmail.com>
2024-05-15 15:42:46 +0300

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,16 +8,31 @@ from typing import Union
 from uuid import uuid4
 
 
-class Cache:
-    """Create a Cache class"""
+class Cache():
+    """Cache class"""
 
-    def __init__(self):
-        """store an instance of the Redis client"""
+    def __init__(self) -> None:
+        """initiates the class with a redis instance"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """generate a random key"""
-        random_key = str(uuid4())
-        self._redis.set(random_key, data)
-        return random_key
+        """Stores a data with a random key"""
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
+
+# class Cache:
+#     """Create a Cache class"""
+
+#     def __init__(self):
+#         """store an instance of the Redis client"""
+#         self._redis = redis.Redis()
+#         self._redis.flushdb()
+
+
+#     def store(self, data: Union[str, bytes, int, float]) -> str:
+#         """generate a random key"""
+#         random_key = str(uuid4())
+#         self._redis.set(random_key, data)
+#         return random_key
